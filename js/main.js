e5a51558e7f702a95213f540cc2df544ff612291
victor ukwuezeh <victorukwuezeh@gmail.com>
2022-04-12 13:39:58 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 const calcScreen = document.getElementById("calc-screen");
 const resultScreen = document.getElementById("result-screen");
-const nob = document.querySelector(".nob");
-
+const nob = document.getElementById("nob");
 const number = document.querySelectorAll(".number");
 const operator = document.querySelectorAll(".operator");
+const body = document.getElementsByTagName('body')[0];
 
 
 let numbers = [];
@@ -17,16 +17,21 @@ number.forEach((num) => {
   num.addEventListener("click", () => {
 
     if (resultScreen.innerHTML.length == 0) {
-      if (calcScreen.innerHTML == 0 && num.id == 0) {
+     
+      if (calcScreen.innerHTML == 0 && num.id == 0 && calcScreen.innerHTML.length <2) {
         calcScreen.innerHTML = 0;
       } else {
         if (calcScreen.innerHTML == 0 && calcScreen.innerHTML.length == 1) {
-          calcScreen.innerHTML = num.id;
         } else {
+
           calcScreen.innerHTML += num.id;
+         
         }
+        
       }
+      console.log(calcScreen.innerHTML)
     } else {
+      
       calcScreen.innerHTML = resultScreen.innerHTML;
       calcScreen.innerHTML = num.id;
       resultScreen.innerHTML = ""
@@ -37,48 +42,47 @@ number.forEach((num) => {
 // operators
 operator.forEach((op) => {
   op.addEventListener("click", () => {
-      
+
     if (resultScreen.innerHTML.length == 0) {
       if (calcScreen.innerHTML.length == 0) {
         calcScreen.innerHTML = "";
       } else {
-    
- 
+
+
 
         if (
           op.id == "=" ||
+          op.id == "nob" ||
           op.id == "sqrt" ||
           op.id == "sqr" ||
           op.id == "bkspace" ||
-          op.id == "onefr"||
+          op.id == "onefr" ||
           op.id == 'plus-minus'
-        ) {
-        } else {
+        ) {} else {
 
-                if (isNaN(calcScreen.innerHTML.substr(calcScreen.innerHTML.length - 1)) == true) {
-                calcScreen.innerHTML =    calcScreen.innerHTML.substring(0,calcScreen.innerHTML.length - 1);
-                calcScreen.innerHTML+=op.id;
-     } else {
-         calcScreen.innerHTML += op.id;
-     }
+          if (isNaN(calcScreen.innerHTML.substr(calcScreen.innerHTML.length - 1)) == true ) {
+            calcScreen.innerHTML = calcScreen.innerHTML.substring(0, calcScreen.innerHTML.length - 1);
+           
+            calcScreen.innerHTML += op.id;
+          } else {
+            calcScreen.innerHTML += op.id;
+          }
 
-       
+
         }
       }
 
       // Plus or Minus
+      if (op.id == 'plus-minus') {
+        let plusMinus = -Number(calcScreen.innerHTML);
+        if (isNaN(plusMinus)) {
 
-        if(op.id == 'plus-minus'){
-              let plusMinus = -Number(calcScreen.innerHTML);
-              if (isNaN(plusMinus)){
-                   
-              } else{
-                calcScreen.innerHTML = plusMinus; 
-                    
+        } else {
+          calcScreen.innerHTML = plusMinus;
 
-              }
+
         }
-
+      }
       // Plus or Minus
 
 
@@ -100,11 +104,12 @@ operator.forEach((op) => {
       if (op.id == "=") {
         let ans = eval(calcScreen.innerHTML);
 
-        resultScreen.innerHTML = Number(ans.toString().substr(0, 10));
-        // console.log(eval(calcScreen.innerHTML));
+        resultScreen.innerHTML = ans.toString().substr(0, 20);
+
       }
       // Equal
-        
+     
+
 
       // Square
       if (op.id == "sqr") {
@@ -115,10 +120,7 @@ operator.forEach((op) => {
       // bkspace
       if (op.id == "bkspace") {
         let bkSpaceStr = calcScreen.innerHTML.toString();
-        calcScreen.innerHTML = bkSpaceStr.substr(
-          0,
-          calcScreen.innerHTML.length - 1
-        );
+        calcScreen.innerHTML = bkSpaceStr.substr(0, calcScreen.innerHTML.length - 1);
       }
       // bkspace
 
@@ -128,6 +130,11 @@ operator.forEach((op) => {
       }
       // onefr
     } else {
+      
+      if (
+        op.id == "nob"
+      ){}
+
       if (op.id != "clear") {
         if (op.id == "sqrt") {
           calcScreen.innerHTML = resultScreen.innerHTML;
@@ -141,17 +148,19 @@ operator.forEach((op) => {
         } else if (op.id == "onefr") {
           calcScreen.innerHTML = resultScreen.innerHTML;
           resultScreen.innerHTML = 1 / calcScreen.innerHTML;
-        } else if(op.id == 'plus-minus'){
-                  calcScreen.innerHTML =  resultScreen.innerHTML;
-                  resultScreen.innerHTML = "";
-                 let plusMinus = -Number(calcScreen.innerHTML);
-              if (isNaN(plusMinus)){
+        } else if (op.id == 'plus-minus') {
+          calcScreen.innerHTML = resultScreen.innerHTML;
+          resultScreen.innerHTML = "";
+          let plusMinus = -Number(calcScreen.innerHTML);
+          if (isNaN(plusMinus)) {
 
-              }else{
-                calcScreen.innerHTML = plusMinus; 
-              }
-                 
-        } else {
+          } else {
+            calcScreen.innerHTML = plusMinus;
+          }
+
+        } else if (op.id == "bkspace") {
+          calcScreen.innerHTML = "";
+        }else {
           calcScreen.innerHTML = resultScreen.innerHTML;
 
 
@@ -165,12 +174,16 @@ operator.forEach((op) => {
     }
   });
 });
-     
+
 nob.addEventListener("click", () => {
-  nob.classList.toggle("switch");
-  document.documentElement.classList.toggle("dark-mode");
-  // document.querySelectorAll(".inverted").forEach((invert) => {
-  //   invert.classList.toggle("dark-mode");
-  // });
+ body.classList.toggle("dark-mode");
+ if(body.getAttribute('class')=="dark-mode"){
+  nob.innerHTML = `<i class="fas fa-sun"></i>`;
+  nob.style.css({
+    'background': 'red'
+  })
+ }else{
+  nob.innerHTML = `<i class="fas fa-moon"></i>`
+ }
 
 });
